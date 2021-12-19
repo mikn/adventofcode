ee50fa5352812d9a3e5d5fb641fcfac9e9da13c1
Mikael Knutsson <mikael.knutsson@gmail.com>
2021-12-20 00:29:55 +0800

    Solution day 1 - star 2

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -1,36 +1,62 @@
 use std::env;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::num::ParseIntError;
 
-fn main() -> Result<(), ParseIntError> {
+struct Values {
+    deq: VecDeque<i64>,
+    max_len: usize,
+}
+
+impl Values {
+    fn sum(&self) -> i64 {
+        let mut ret = 0;
+        for val in self.deq.iter() {
+            ret += val;
+        }
+        return ret;
+    }
+
+    fn len(&self) -> usize {
+        return self.deq.len();
+    }
+
+    fn add(&mut self, x: i64) {
+        if self.deq.len() == self.max_len {
+            self.deq.pop_front();
+        }
+        self.deq.push_back(x);
+    }
+
+    fn new(max_len: usize) -> Values {
+        Values{ deq: VecDeque::with_capacity(max_len), max_len: max_len }
+    }
+}
+
+fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     println!("Reading file {}", filename);
 
     let f = File::open(filename).unwrap();
 
-    let mut file = BufReader::new(&f);
+    let file = BufReader::new(&f);
 
-    let mut first_line = String::new();
-    file.read_line(&mut first_line).unwrap();
-
-    let mut prev_val = match first_line.trim().parse::<i64>() {
-        Ok(prev_val) => prev_val,
-        Err(e) => {
-            println!("line {} produced error {}", first_line, e);
-            return Err(e);
-        },
-    };
+    let mut vals = Values::new(3);
     let mut inc_count = 0;
+
     for line in file.lines() {
         let val = line.unwrap().trim().parse::<i64>().unwrap();
-        if val > prev_val {
-            inc_count += 1;
+        if vals.len() == vals.max_len {
+            let prev_sum = vals.sum();
+            vals.add(val);
+            if prev_sum < vals.sum() {
+                inc_count += 1;
+            }
+        } else {
+            vals.add(val);
         }
-        prev_val = val;
     }
     println!("The value increased {} times from the previous value!", inc_count);
-    Ok(())
 }
