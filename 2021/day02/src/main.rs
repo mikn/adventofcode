cb15b0080cf576d5d3c2b784079e1a9357d84817
Mikael Knutsson <mikael.knutsson@gmail.com>
2021-12-20 00:30:18 +0800

    Solution day 2 - star 1

---
+++ 2021/day02/src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    println!("Reading file {}", filename);
+
+    let f = File::open(filename).unwrap();
+
+    let file = BufReader::new(&f);
+
+    let mut depth = 0;
+    let mut x = 0;
+
+    for line in file.lines() {
+        let l = line.unwrap();
+        let vals = l.trim().split_whitespace().collect::<Vec<&str>>();
+        let val = vals[1].parse::<i64>().unwrap();
+        match vals[0] {
+            "forward" => x += val,
+            "up" => depth -= val,
+            "down" => depth += val,
+            _ => panic!("Command {} not supported!", vals[0]),
+        }
+    }
+    println!("The product of depth and horizontal position is: {}!", depth*x);
+}
